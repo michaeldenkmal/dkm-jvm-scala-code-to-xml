21eb523eaac0c1c379cb6642bdc6c1bc75c1b7d6
michaeldenkmal <michael@denkmal.at>
2024-04-28 22:18:12 +0200

    scalaCodeToXml.bat erstellt für Aufruf aus Python code gen

--- scalaCodeToXml.bat
+++ scalaCodeToXml.bat
@@ -1,6 +1,7 @@
 rem TODO
-set code_file=D:\projekte\oh\oh_java_mv\wa_oh\src\main\scala\at\denkmal\libohws\ws\GpRes.scala
-set xml_file=%code_file%.xml
+set code_file=%1
+set xml_file=%2
 set jar=.\target\scala-2.13\scalaCodeToJson-assembly-0.1.0-SNAPSHOT.jar
 set main_class=at.denkmal.scala_code_to_xml.Main
+cd /D %~dp0
 java -cp %jar% %main_class% %code_file% %xml_file%
